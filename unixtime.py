78074e20f7b1a6f3183e0427ebfd66bec89f9519
649748661@qq.com <649748661@qq.com>
2020-03-14 19:17:51 +0800

    test

--- unixtime.py
+++ unixtime.py
@@ -5,14 +5,20 @@ from datetime import datetime
 import time
 
 
-def get_unix_timestamp():
+def get_timestamp():
     return int(time.time())
 
-def get_milli_unix_timestamp():
+
+def get_milli_timestamp():
     return int(round(time.time() * 1000))
 
 
-def date_to_timestamp(strftime=None, date=None):
+def get_date(strftime=None):
+    strftime = '%Y-%m-%d %H:%M:%S' if strftime is None else strftime
+    return datetime.utcnow().strftime(strftime)
+
+
+def unix_timestamp(strftime=None, date=None):
     strftime = '%Y-%m-%d %H:%M:%S' if strftime is None else strftime
     if date == None:
         return int(time.time())
@@ -23,7 +29,7 @@ def date_to_timestamp(strftime=None, date=None):
     return timestamp
 
 
-def timestamp_to_date(strftime=None, timestamp=None):
+def from_unixtime(strftime=None, timestamp=None):
     strftime = '%Y-%m-%d %H:%M:%S' if strftime is None else strftime
     timestamp = int(time.time()) if timestamp is None else timestamp
     date = time.localtime(timestamp)
@@ -32,9 +38,12 @@ def timestamp_to_date(strftime=None, timestamp=None):
 
 
 if __name__ == '__main__':
-    print(get_unix_timestamp())
-    print(get_milli_unix_timestamp())
-    print(date_to_timestamp(date="2020-03-14 0:0:0"))
-    print(date_to_timestamp(strftime="%Y-%m-%dT%H:%M:%S"))
-    print(timestamp_to_date(timestamp=1584154137))
-    print(timestamp_to_date(strftime='%Y-%m-%dT%H:%M:%S'))
+    print(get_timestamp())
+    print(get_milli_timestamp())
+    print(get_date())
+    print(unix_timestamp())
+    print(unix_timestamp(strftime="%Y-%m-%dT%H:%M:%S"))
+    print(unix_timestamp(date="2020-03-14 0:0:0"))
+    print(from_unixtime())
+    print(from_unixtime(strftime='%Y-%m-%d,%H:%M:%S'))
+    print(from_unixtime(timestamp=1584154137))
